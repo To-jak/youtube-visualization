d98d950868daf0b8f4fd99041bffaa7b2351c2d4
btayart <brice_tayart@yahoo.fr>
2020-06-19 16:48:47 +0200

    Add dropdown to select statistics Change slider to select trending dates

--- youtube-visualization/js/yt-visualization.js
+++ youtube-visualization/js/yt-visualization.js
@@ -37,6 +37,16 @@ var category_id_to_name = {
 }
 
 // Loading Data /////////////////////////////////////////////////
+/*
+Data is laoded here.
+Empty categories are replaced with "".
+Tags are parsed and split
+
+Upon completion and loading:
+- initialization functions are called once
+- drawing functions are called to update the viz
+*/
+
 
 let dataset = [];
 function onlyUnique(value, index, self) {
@@ -310,36 +320,58 @@ This is the code for the heatmap on the top left of the screen
 Main functions:
 init_trend_heatmap() called once to initialize some values from dataset
 draw_trend_heatmap() called to update the drawing according to selection
+
+object trend contains global variables that are linked to the thrend heatmap
 */
 
-//temporary placeholders
+//temporary placeholders for dev
 // function init_trend_heatmap() { } 
 // function draw_trend_heatmap() { }
 
 var trend = {
+    select_height:20,
     height: document.getElementById("TagTrends").clientHeight,
     width: document.getElementById("TagTrends").clientWidth,
-    margins: {top:8, bottom:18, left:120, right:8}};
-trend.in_height = trend.height - trend.margins.top - trend.margins.bottom;
+    margins: {top:8, bottom:18, left:120, right:8},
+    current_metric: "count"
+};
+trend.in_height = trend.height - trend.margins.top - trend.margins.bottom - trend.select_height;
 trend.in_width =  trend.height - trend.margins.left - trend.margins.right;
 
-let svg_trend = d3.select("#TagTrends")
-    .append("svg")
-    .attr("width",trend.width)
-    .attr("height",trend.height)
-    .append("g")
-    .attr("transform",
-    "translate(" + trend.margins.left + "," + trend.margins.top + ")");    
+var metrics = ["count",
+    "total_views", "total_dislikes", "total_dislikes",
+    "avg_views","avg_likes","avg_dislikes",
+    "likes_per_view", "dislikes_per_view", "like_ratio"]
+
 
 // Init function called once to initialize some values
+var select_trend = d3.select("#TagTrends")
+        .append("select");
+var svg_trend = d3.select("#TagTrends")
+        .append("svg")        .attr("width",trend.width)
+        .attr("height",trend.height)
+        .append("g")
+        .attr("transform",
+        "translate(" + trend.margins.left + "," + trend.margins.top + ")");
 var unique_categories = [];
 var max_duration = -1;
 
 trend.xscale = d3.scaleBand();
 trend.yscale = d3.scaleBand();
-//trend.cat_scale = d3.scaleOrdinal();
 
 function init_trend_heatmap(){
+    // Set-up graphic elements
+    select_trend
+        .attr("class","dropdown")
+        .attr("id","trend_dropdown")
+        .on("change", dropdownChange)
+        .selectAll("option")
+        .data(metrics)
+        .enter()
+        .append("option")
+        .attr("value",d=>d)
+        .text(d=>d.replace(/_/g," "));
+
     // largest trending duration (for scale)
     max_duration = d3.max(
         dataset,
@@ -374,6 +406,7 @@ function init_trend_heatmap(){
 
 }
 
+/*
 // create a tooltip
 trend.tooltip = d3.select("body")
     .append("div")
@@ -384,9 +417,16 @@ trend.tooltip = d3.select("body")
     .style("border-width", "2px")
     .style("border-radius", "5px")
     .style("padding", "5px")
+*/
 
+// Drop down callback
+function dropdownChange(){
+    trend.current_metric = d3.select(this).property('value');
+    console.log("current metric is now "+trend.current_metric)
+    draw_trend_heatmap();
+}
 
-// Three function that change the tooltip when user hover / move / leave a cell
+// Mouse callbacks for tootip update
 trend.mouseover = function(d) {
         cat_tooltip.style("opacity", .9)
     }
@@ -399,7 +439,13 @@ trend.mousemove = function(d) {
             "<b>" +  d.count + "</b> videos have been trending for <b>"+d.trend_duration+"</b> days<br>"+
             "Total views : <b>"+ d.total_views+"</b><br>"+
             "Total likes : <b>"+ d.total_likes+"</b><br>"+
-            "Total dislikes : <b>"+ d.total_dislikes+"</b>")
+            "Total dislikes : <b>"+ d.total_dislikes+"</b><br>"+
+            "Avg views per video : <b>"+ d.avg_views.toFixed(0)+"</b><br>"+
+            "Avg likes per video : <b>"+ d.avg_likes.toFixed(0)+"</b><br>"+
+            "Avg dislikes per video : <b>"+ d.avg_dislikes.toFixed(0)+"</b><br>"+
+            "Avg likes per view : <b>"+ d.likes_per_view.toFixed(3)+"</b><br>"+
+            "Avg dislikes per view : <b>"+ d.dislikes_per_view.toFixed(3)+"</b><br>"+
+            "Ratio of likes among reactions : <b>"+ d.like_ratio.toFixed(3)+"</b>")
         .style("left", (d3.event.pageX) + "px")
         .style("top", (d3.event.pageY+70) + "px")
     }
@@ -407,7 +453,13 @@ trend.mouseleave = function(d) {
         cat_tooltip.style("opacity", 0)
     }
 
+// Main update function
 function draw_trend_heatmap(){
+    /*
+    Here, the data is first processed to give an array that gives a list of statistics
+    for each category and each trend duration
+    */
+
 
     // Reduction on the dataset
     // Define some functions to access metrics of interest
@@ -452,23 +504,31 @@ function draw_trend_heatmap(){
         );
     flat_metrics = [].concat.apply([], flat_metrics);    
 
-    // Now, to the map!
-    metric="count"
+    flat_metrics = Array.from(flat_metrics,
+        function (d){
+            out={...d,
+                likes_per_view:d.total_likes/d.total_views,
+                dislikes_per_view:d.total_dislikes/d.total_views,
+                like_ratio:(d.total_likes+.5)/(d.total_likes+d.total_dislikes+1)
+                };
+                return out;
+            })
+
+    // Preprocesing is over. Now, to the heatmap!
+
+    // Currently selected metric
+    let metric = trend.current_metric;
     
     // Build color scale
-    var myColor = d3.scaleSequential(d3.interpolateYlGnBu)
-    .domain([0 , 1000]);//d3.max(flat_metrics,d=>d[metric])]);
+    metric_extent = d3.extent(flat_metrics, d=>d[metric]);
+    var trendColors = d3.scaleSequential(d3.interpolateYlGnBu)
+    .domain([0 , metric_extent[1]]);//d3.max(flat_metrics,d=>d[metric])]);
     
-
-
-    console.log(flat_metrics)
-    console.log(trend.xscale.bandwidth() )
-    console.log(trend.yscale.bandwidth() )
     //Heatmap
     boxes = svg_trend.selectAll()
         .data(flat_metrics, function(d) {return d.category+'|'+d.trend_duration;});
     
-    boxes.style("fill", function(d) { return myColor(d[metric])} );
+    boxes.style("fill", function(d) { return trendColors(d[metric])} );
     boxes.enter()
         .append("rect")
         .attr("x", function(d) {
@@ -479,11 +539,11 @@ function draw_trend_heatmap(){
             })
         .attr("width", trend.xscale.bandwidth() )
         .attr("height", trend.yscale.bandwidth() )
-        .style("fill", function(d) { return myColor(d[metric])} )
+        .style("fill", function(d) { return trendColors(d[metric])} )
         .on("mouseover", trend.mouseover)
         .on("mousemove", d=> trend.mousemove(d))
         .on("mouseleave", trend.mouseleave);
-    boxes.exit().remove;
+    boxes.exit().selectAll("rect").remove();
 }
 
 
@@ -501,14 +561,14 @@ date_formatter = d3.timeFormat("%x");// %x is locale format for dates
 
 function filter_by_time(d) {
     // Filter data entries with .filter(filter_by_time)
-    let c1 = d.publish_date >= date_range[0];
-    let c2 = d.publish_date <= date_range[1];
+    let c1 = d.trending_date >= date_range[0];
+    let c2 = d.trending_date <= date_range[1];
     return  c1 && c2
 }
 
 function init_timeline_range() {
     // Call upon loading the dataset to set the scale range
-    date_scale.domain(d3.extent(dataset, d => d.publish_date));
+    date_scale.domain(d3.extent(dataset, d => d.trending_date));
     slider.range(0,SLIDE_MAX);
 }
 
